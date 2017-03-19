119539ee39de3385853f3432f27036222add5aaa
Petr Kubat <kubatpe1@gmail.com>
2017-03-20 00:23:20 +0100

    More comments and examples.

--- src/test/SortTest.scala
+++ src/test/SortTest.scala
@@ -1,6 +1,5 @@
 package test
 
-import functionadaptors.FunctionAdaptor1
 import functionadaptors.Implicits._
 import options.Measurement
 
@@ -8,11 +7,10 @@ import options.Measurement
   * Created by pk250187 on 3/19/17.
   */
 
-class Foo[T, R] {
-  def run(fun: T => R) = ???
-}
-
 object SortTest {
+  // -------------------------
+  // Functional usage example:
+  // -------------------------
   def selectionSort(data: List[Int]): List[Int] = ???
   def quickSort(data: List[Int]): List[Int] = ???
   def bubbleSort(data: List[Int]): List[Int] = ???
@@ -23,26 +21,30 @@ object SortTest {
   // val sort3 = selectionSort _ or quickSort or bubbleSort by (_.size) using Measurement.RunTime
   // val sort4 = (selectionSort _).or(quickSort).or(bubbleSort).by(_.size).using(Measurement.RunTime)
 
+  // Why do the following examples compile? It looks like the same kind of conversion / eta expansion
   val chain = (selectionSort _).andThen(quickSort).andThen(bubbleSort)
 
   def run[T, R](fun: T => R) = ???
   run(selectionSort)
 
+  class Foo[T, R] {
+    def run(fun: T => R) = ???
+  }
+
   val x = new Foo[List[Int], List[Int]]()
   x.run(selectionSort)
 
-  val adaptor: FunctionAdaptor1[List[Int], List[Int]] = new FunctionAdaptor1(List())
-  adaptor.or(quickSort _)
-
   List(List(1,2), List(10, 2)).map(selectionSort)
-}
 
-// OOP Example:
-object Sortable {
-  implicit class SortableList[T](list: List[T]) {
-    def selectionSort(): List[Int] = ???
-    def quickSort(): List[Int] = ???
-    def bubbleSort(): List[Int] = ???
-    val sort = selectionSort _ or quickSort _ or bubbleSort _ by (() => list.size) using Measurement.RunTime
+  // ------------------
+  // OOP usage example:
+  // ------------------
+  object Sortable {
+    implicit class SortableList[Int](list: List[Int]) {
+      def selectionSort(): List[Int] = ???
+      def quickSort(): List[Int] = ???
+      def bubbleSort(): List[Int] = ???
+      val sort = selectionSort _ or quickSort _ or bubbleSort _ by (() => list.size) using Measurement.RunTime
+    }
   }
 }
