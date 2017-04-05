4debe5aa2a1d007d9476ab5f666fe754412c05ef
Petr Kubat <kubatpe1@gmail.com>
2017-04-05 23:01:11 +0200

    Added methods vs. functions test object.

---
+++ src/test/Methods.scala
@@ -0,0 +1,23 @@
+package test
+
+/**
+  * Created by pk250187 on 4/2/17.
+  */
+object Methods {
+  def main(args: Array[String]): Unit = {
+    class Class {
+      def method(arg: String): String = s"Called method($arg) on $this"
+
+      val methodFunction: (String) => String = arg => method(arg)
+
+      def foo() = List("One", "Two", "Three").map(method)
+    }
+    val obj = new Class()
+    println(obj.method("Hello"))
+
+    val function: (String) => Unit = arg => println(obj.method(arg))
+    function("Hello again!")
+    List("One", "Two", "Three").foreach(function)
+    obj.foo().foreach(println)
+  }
+}
