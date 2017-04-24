19e9ca08839892b8b5d5736473c6368254c2af96
Petr Kubat <kubatpe1@gmail.com>
2017-04-24 22:43:48 +0200

    Added new tests for the InterpolationSelector.

--- Tests/src/Test.scala
+++ Tests/src/Test.scala
@@ -10,6 +10,8 @@ object Test {
   lazy val smallData = Seq.fill(smallDataSize)(Random.nextInt).toList
   lazy val bigData = Seq.fill(bigDataSize)(Random.nextInt).toList
 
+  def customData(size: Int): List[Int] = Seq.fill(size)(Random.nextInt).toList
+
   def isOrdered(l : List[Int]): Boolean = (l, l.tail).zipped.forall(_ <= _)
 
   def runTest(testFnc: (List[Int]) => List[Int]): Unit = {
@@ -21,11 +23,25 @@ object Test {
     })
   }
 
+  def runIncrementalTest(testFnc: (List[Int]) => List[Int]): Unit = {
+    Seq.range(0, runCount).foreach(i => {
+      Seq.range(0, 1).foreach(j => {
+        val data = customData(smallDataSize + i)
+        println(s"Running test no: ${i} on ${data.size}")
+        val res = testFnc(data)
+        assert(isOrdered(res))
+      })
+    })
+  }
+
   def main(args: Array[String]): Unit = {
     import Sortable._
 
     //runTest(l => l.sort())
     val sorter = new Sorter()
-    runTest(l => sorter.sort(l))
+    //runTest(l => sorter.sort(l))
+    Seq.range(0, 1).foreach(i => {
+      runIncrementalTest(l => sorter.sort(l))
+    })
   }
 }
