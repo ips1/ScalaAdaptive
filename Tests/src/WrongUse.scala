d97fc71cb7dd2889a93af2986f0fec0dae9712d2
Petr Kubat <kubatpe1@gmail.com>
2017-04-29 20:58:13 +0200

    Added wrong use example.

---
+++ Tests/src/WrongUse.scala
@@ -0,0 +1,12 @@
+import scalaadaptive.core.options.Storage
+
+/**
+  * Created by pk250187 on 4/29/17.
+  */
+object WrongUse {
+  import scalaadaptive.core.macros.AdaptiveMacros._
+  def impl1(data: List[Int]): Int = ???
+  def impl2(data: List[Int]): Int = ???
+
+  def processData(data: List[Int]): Int = (impl1 _ or impl2 withStorage Storage.Local)(data)
+}
