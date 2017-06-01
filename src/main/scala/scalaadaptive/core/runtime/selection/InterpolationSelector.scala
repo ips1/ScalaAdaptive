2e2a253dec567a26dc25e766aaa9cc0c04f1e4b1
Petr Kubat <kubatpe1@gmail.com>
2017-06-01 21:00:52 +0200

    Improved InterpolationSelector, fixed NumberIsTooSmallException.

--- src/main/scala/scalaadaptive/core/runtime/selection/InterpolationSelector.scala
+++ src/main/scala/scalaadaptive/core/runtime/selection/InterpolationSelector.scala
@@ -1,7 +1,10 @@
 package scalaadaptive.core.runtime.selection
 
 import org.apache.commons.math3.analysis.interpolation.{LinearInterpolator, LoessInterpolator}
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction
+import org.apache.commons.math3.exception.NumberIsTooSmallException
 
+import scalaadaptive.core.runtime.history.rundata.GroupedRunData
 import scalaadaptive.core.runtime.history.runhistory.RunHistory
 
 /**
@@ -9,6 +12,48 @@ import scalaadaptive.core.runtime.history.runhistory.RunHistory
   */
 class InterpolationSelector[TMeasurement](implicit num: Numeric[TMeasurement])
   extends RunSelector[TMeasurement] {
+
+  // TODO : Chose interpolator
+  private def getInterpolator = new LoessInterpolator()
+
+  /**
+    * Interpolates polynomial, returns the function in case of success, returns None if there is not enough data
+    * @param sortedData
+    * @return
+    */
+  private def interpolate(sortedData: Seq[(Option[Long], GroupedRunData[TMeasurement])]): Option[PolynomialSplineFunction] = {
+    try {
+      Some(
+        getInterpolator.interpolate(
+          sortedData.map(i => i._1.get.toDouble).toArray,
+          sortedData.map(i => num.toDouble(i._2.averageRunData.measurement)).toArray
+        )
+      )
+    } catch {
+      // TODO: Handle more exceptions
+      case ex: NumberIsTooSmallException => None
+    }
+  }
+
+  private def selectUsingInterpolation(records: Seq[RunHistory[TMeasurement]],
+                                       descriptor: Long) = {
+    val polynomials =
+      records.map(runHistory => {
+        val sortedData = runHistory.runAveragesGroupedByDescriptor
+          .toList
+          .filter(i => i._1.isDefined)
+          .sortBy(i => i._1)
+        (runHistory, interpolate(sortedData))
+      })
+
+    val min = polynomials.minBy(p => if (p._2.isEmpty || !p._2.get.isValidPoint(descriptor)) {
+      // TODO: Replace by logger
+      println("Invalid point!")
+      -1
+    } else p._2.get.value(descriptor))
+    min._1
+  }
+
   override def selectOption(records: Seq[RunHistory[TMeasurement]],
                             inputDescriptor: Option[Long]): RunHistory[TMeasurement] = {
     logger.log("Selecting using InterpolationSelector")
@@ -18,32 +63,6 @@ class InterpolationSelector[TMeasurement](implicit num: Numeric[TMeasurement])
       case _ => return records.head
     }
 
-    // TODO : Chose interpolator
-    //val interpolator = new LinearInterpolator()
-    val interpolator = new LoessInterpolator()
-
-    // TODO: Exceptions thrown from interpolator!
-    // Should not be used on ungrouped data!
-    // TODO: Exception in thread "main" org.apache.commons.math3.exception.NumberIsTooSmallException: number of points (1)
-    val polynomials =
-      records.map(runHistory => {
-        val sortedData = runHistory.runAveragesGroupedByDescriptor
-          .toList
-          .filter(i => i._1.isDefined)
-          .sortBy(i => i._1)
-        (runHistory,
-         interpolator.interpolate(
-           sortedData.map(i => i._1.get.toDouble).toArray,
-           sortedData.map(i => num.toDouble(i._2.averageRunData.measurement)).toArray
-         )
-        )
-      })
-
-    val min = polynomials.minBy(p => if (!p._2.isValidPoint(descriptor)) {
-      println("Invalid point!")
-      -1
-    } else p._2.value(descriptor))
-
-    min._1
+    selectUsingInterpolation(records, descriptor)
   }
 }
