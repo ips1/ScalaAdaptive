45728b9d61e1796b4f41ca8c9361798ea2d5c0fe
Petr Kubat <kubatpe1@gmail.com>
2017-06-06 09:56:06 +0200

    AdaptiveInternal implementation moved to a trait.

--- src/main/scala/scalaadaptive/api/Adaptive.scala
+++ src/main/scala/scalaadaptive/api/Adaptive.scala
@@ -1,7 +1,7 @@
 package scalaadaptive.api
 
 import scalaadaptive.core.configuration.Configuration
-import scalaadaptive.core.runtime.AdaptiveInternal
+import scalaadaptive.core.runtime.{AdaptiveCore, AdaptiveInternal}
 
 /**
   * Created by pk250187 on 6/3/17.
@@ -10,6 +10,8 @@ import scalaadaptive.core.runtime.AdaptiveInternal
   * Global API object that allows the library user to initialize the entire framework.
   */
 object Adaptive {
+  val internal: AdaptiveCore = AdaptiveInternal
+
   /**
     * Initializes the ScalaAdaptive framework with new configuration
     * Note that some parts of the configuration (e.g. the default configuration of the MultiFunctions) will have effect
@@ -18,11 +20,11 @@ object Adaptive {
     * @param configuration The configuration that will be used in the framework.
     *                      For more information, see Configuration class.
     */
-  def initialize(configuration: Configuration): Unit = AdaptiveInternal.initialize(configuration)
+  def initialize(configuration: Configuration): Unit = internal.initialize(configuration)
 
   /**
     * Re-initializes the ScalaAdaptive framework with new configuration
     * Note that all the run history data that were stored using Storage.Global will be lost.
     */
-  def reset(): Unit = AdaptiveInternal.reset()
+  def reset(): Unit = internal.reset()
 }
---
+++ src/main/scala/scalaadaptive/core/runtime/AdaptiveCore.scala
@@ -0,0 +1,83 @@
+package scalaadaptive.core.runtime
+
+import scalaadaptive.analytics.{AnalyticsSerializer, BasicAnalyticsData, CsvAnalyticsSerializer}
+import scalaadaptive.core.functions.adaptors.FunctionConfig
+import scalaadaptive.core.configuration.Configuration
+import scalaadaptive.core.configuration.defaults.FullHistoryTTestConfiguration
+import scalaadaptive.core.functions.analytics.{AnalyticsCollector, BasicAnalyticsCollector}
+import scalaadaptive.core.logging.LogManager
+import scalaadaptive.core.functions.references.CustomIdentifierValidator
+import scalaadaptive.core.runtime.history._
+import scalaadaptive.core.functions.{DefaultFunctionFactory, FunctionFactory}
+
+/**
+  * Created by pk250187 on 3/19/17.
+  */
+trait AdaptiveCore {
+  private val defaultConfiguration = new FullHistoryTTestConfiguration
+  private var currentConfiguration: Configuration = defaultConfiguration
+
+  private def initOptionRunner(configuration: Configuration): AdaptiveRunner = {
+    new HistoryBasedAdaptiveRunner[configuration.TMeasurement](
+      configuration.createHistoryStorage(),
+      configuration.createDiscreteRunSelector(),
+      configuration.createContinuousRunSelector(),
+      configuration.createPerformanceProvider(),
+      configuration.createGroupSelector(),
+      configuration.createLogger())
+  }
+
+  private def initPersistentOptionRunner(configuration: Configuration): Option[AdaptiveRunner] =
+    configuration.createPersistentHistoryStorage().map(persistentStorage =>
+      new HistoryBasedAdaptiveRunner[configuration.TMeasurement](
+        persistentStorage,
+        configuration.createDiscreteRunSelector(),
+        configuration.createContinuousRunSelector(),
+        configuration.createPerformanceProvider(),
+        configuration.createGroupSelector(),
+        configuration.createLogger()
+      )
+    )
+
+  private var identifierValidator: CustomIdentifierValidator = defaultConfiguration.createIdentifierValidator()
+  private var multiFunctionDefaults: FunctionConfig = defaultConfiguration.createMultiFunctionDefaultConfig()
+  private var functionFactory: FunctionFactory = defaultConfiguration.createFunctionFactory()
+  private var analyticsSerializer: AnalyticsSerializer = defaultConfiguration.createAnalyticsSerializer()
+  private var runner: AdaptiveRunner = initOptionRunner(defaultConfiguration)
+  private var persistentRunner: AdaptiveRunner = initPersistentOptionRunner(defaultConfiguration).getOrElse(runner)
+
+  def getIdentifierValidator: CustomIdentifierValidator = identifierValidator
+
+  def getMultiFunctionDefaults: FunctionConfig = multiFunctionDefaults
+
+  def getSharedRunner: AdaptiveRunner = runner
+
+  def getSharedPersistentRunner: AdaptiveRunner = persistentRunner
+
+  def getFunctionFactory: FunctionFactory = functionFactory
+
+  def getAnalyticsSerializer: AnalyticsSerializer = analyticsSerializer
+
+  def createAnalytics(): AnalyticsCollector = currentConfiguration.createAnalyticsCollector()
+
+  def createNewRunner(): AdaptiveRunner =
+    initOptionRunner(currentConfiguration)
+
+  def initialize(configuration: Configuration): Unit = {
+    currentConfiguration = configuration
+    LogManager.setLogger(currentConfiguration.createLogger())
+    runner = initOptionRunner(currentConfiguration)
+    persistentRunner = initPersistentOptionRunner(currentConfiguration).getOrElse(runner)
+    identifierValidator = currentConfiguration.createIdentifierValidator()
+    multiFunctionDefaults = currentConfiguration.createMultiFunctionDefaultConfig()
+    functionFactory = currentConfiguration.createFunctionFactory()
+    analyticsSerializer = currentConfiguration.createAnalyticsSerializer()
+  }
+
+  def reset(): Unit = {
+    initialize(currentConfiguration)
+  }
+
+  // Default initialization
+  reset()
+}
