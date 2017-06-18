cbb43974e9fd569bd6144da35971af577b6d0c64
Petr Kubat <kubatpe1@gmail.com>
2017-06-19 01:21:53 +0200

    Added documentation to the AnalyticsData.

--- src/main/scala/scalaadaptive/analytics/AnalyticsData.scala
+++ src/main/scala/scalaadaptive/analytics/AnalyticsData.scala
@@ -6,8 +6,22 @@ import java.io.{File, FileOutputStream, PrintWriter}
   * Created by pk250187 on 6/5/17.
   */
 trait AnalyticsData {
+  /**
+    * Retrieves collection of analytic records of all runs of the MultiFunction that this data come from.
+    * @return Collection of AnalyticRecords
+    */
   def getAllRunInfo: Seq[AnalyticsRecord]
+
+  /**
+    * Saves the data in a csv format (separated by semicolons) using the specified PrintWriter
+    * @param out PrintWriter used to save the data
+    */
   def saveData(out: PrintWriter)
+
+  /**
+    * Saves the data in a csv format (separated by semicolons) int the specified File
+    * @param file File to save the data to
+    */
   def saveData(file: File): Unit = {
     file.getParentFile.mkdirs()
     val writer = new PrintWriter(new FileOutputStream(file, false))
