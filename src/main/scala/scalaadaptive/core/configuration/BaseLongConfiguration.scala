39d7f61f96c39b7a11ff880e359b7af1a024845b
Petr Kubat <kubatpe1@gmail.com>
2017-07-17 12:04:25 +0200

    Added Scaladoc to BaseLongConfiguration.

--- src/main/scala/scalaadaptive/core/configuration/BaseLongConfiguration.scala
+++ src/main/scala/scalaadaptive/core/configuration/BaseLongConfiguration.scala
@@ -1,18 +1,30 @@
 package scalaadaptive.core.configuration
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.Path
 
-import scalaadaptive.core.runtime.history._
-import scalaadaptive.core.runtime.history.serialization.{BasicFileNameForKeyProvider, BasicHistorySerializer, HistorySerializer, LongEvaluationDataSerializer}
-import scala.collection.mutable.ArrayBuffer
 import scalaadaptive.core.runtime.history.historystorage.PersistentHistoryStorage
+import scalaadaptive.core.runtime.history.serialization.{BasicFileNameForKeyProvider, BasicHistorySerializer, LongEvaluationDataSerializer}
 import scalaadaptive.extensions.AverageableImplicits.LongIsAverageable
 
 /**
   * Created by Petr Kubat on 4/22/17.
+  *
+  * Extends the [[scalaadaptive.core.configuration.BaseConfiguration]] by fixing the TMeasurement type to [[Long]]
+  * to be able to set up a persistent storage.
+  *
+  * Uses the following implementations for the persistent storage:
+  * [[scalaadaptive.core.runtime.history.historystorage.PersistentHistoryStorage]],
+  * [[scalaadaptive.core.runtime.history.serialization.BasicHistorySerializer]],
+  * [[scalaadaptive.core.runtime.history.serialization.BasicFileNameForKeyProvider]],
+  * [[scalaadaptive.core.runtime.history.serialization.LongEvaluationDataSerializer]]
+  *
   */
 trait BaseLongConfiguration extends BaseConfiguration {
   type TMeasurement = Long
+  /**
+    * The root path to the persisted histories, used as an argument of
+    * [[scalaadaptive.core.runtime.history.serialization.BasicHistorySerializer]].
+    */
   protected val rootPath: Path
 
   override val num = LongIsAverageable
